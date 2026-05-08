2f1b5351fe13345665ad22ae123620f06e614dd6
seveneightfive <35607452+seveneightfive@users.noreply.github.com>
2026-05-08 14:38:50 -0500

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,1 +1,11 @@
-login page
+'use client'
+
+import LoginForm from '@/components/auth/LoginForm'
+
+export default function LoginPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <LoginForm />
+    </main>
+  )
+}
