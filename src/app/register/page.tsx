610b731e2e253dd80ddd84b12baf9b11b7732524
seveneightfive <35607452+seveneightfive@users.noreply.github.com>
2026-05-08 14:39:46 -0500

    Create page.tsx

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import RegisterForm from '@/components/auth/RegisterForm'
+
+export default function RegisterPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <RegisterForm />
+    </main>
+  )
+}
